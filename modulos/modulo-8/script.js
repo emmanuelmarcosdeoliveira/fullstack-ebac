6cf7c8b7d3124683ee17b0505dbc540ed4fbfc64
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-03-22 12:45:03 -0300

    file update script.js

--- modulos/modulo-8/script.js
+++ modulos/modulo-8/script.js
@@ -3,13 +3,25 @@ const imgAprovado =
   '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
 const imgReprovado =
   '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
+const atividades = [];
+const notas = [];
+
 let linhas = "";
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
+  adicionaLinha();
+  atualizaTabela();
+  atualizaMediaFinal();
+});
+
+function adicionaLinha() {
   const inputnomeAtividade = document.getElementById("atv");
   const inputnotaAtividade = document.getElementById("natv");
+
+  atividades.push(inputnomeAtividade.value);
+  notas.push(parseFloat(inputnotaAtividade.value));
 
   let linha = "<tr>";
   linha += `<td>${inputnomeAtividade.value}</td>`;
@@ -17,10 +29,28 @@ form.addEventListener("submit", function (e) {
   linha += `<td>${
     inputnotaAtividade.value >= 7 ? imgAprovado : imgReprovado
   }</td>`;
-  linha += `</tr>`;
+  linha += "</tr>";
 
   linhas += linha;
+  inputnomeAtividade.value = "";
+  inputnotaAtividade.value = "";
+}
 
+function atualizaTabela() {
   const corpoTabela = document.querySelector("tbody");
   corpoTabela.innerHTML = linhas;
-});
+}
+
+function atualizaMediaFinal() {
+  const mediaFinal = caculaMediaFinal();
+  console.log(media);
+}
+
+function caculaMediaFinal() {
+  let somaDasNotas = 0;
+  for (let i = 0; i < notas.length; i++) {
+    somaDasNotas += notas[i];
+  }
+
+  return somaDasNotas / notas.length;
+}
