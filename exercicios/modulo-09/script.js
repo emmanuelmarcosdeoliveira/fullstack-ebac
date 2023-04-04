e594c362a1ac094452ba4aee3984ff82f7428a24
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-04-04 10:48:06 -0300

    finish exercise

--- exercicios/modulo-09/script.js
+++ exercicios/modulo-09/script.js
@@ -1,4 +1,16 @@
-$('form').on ('#botao', function(e){
-e.prevendDefault();
-})
+
+
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+   
+
+    const novaTarefa = $("#tarefa").val();
+    const novoItem = $("<li></li>");
+    $(`<li>${novaTarefa}</li>`).appendTo(novoItem);
+      $(novoItem).appendTo("ul").val();
+      $('#tarefa').val('')
+      $("li").click(function(){
+      $('li').css("text-decoration", 'line-through');
+      });
+    })
 
