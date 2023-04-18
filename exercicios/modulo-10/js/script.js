1245ff6eebe46b4b798d7e8965cbbd3be63841e5
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-04-18 18:21:04 -0300

    udate class 06

--- exercicios/modulo-10/js/script.js
+++ exercicios/modulo-10/js/script.js
@@ -3,3 +3,9 @@ $('div.container #cpf').mask('000-000-000.00'),
 $('div.container #cep').mask('000-00-000' ,{
   placeholder: '000-00-000'
 })
+
+
+const form = document.getElementById("form");
+form.addEventListener('submit', function(e){
+e.preventDefault();
+})
